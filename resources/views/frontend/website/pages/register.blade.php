8fdee71ed47fc02a767aa3e7918ecbdcb03b8c79
Taquthh <quthbiofficial@gmail.com>
2024-11-29 09:19:51 +0800

    Added a telephone form on the register page to adapt to Leconfe 1.1.2

--- resources/views/frontend/website/pages/register.blade.php
+++ resources/views/frontend/website/pages/register.blade.php
@@ -142,6 +142,31 @@
                             </div>
 
                             <div class="space-y-2">
+                                <div class="relative">
+                                    <label class="block text-sm font-medium text-gray-700">
+                                        {{ __('general.phone') }}
+                                    </label>
+                                    <div class="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                                        </svg>
+                                    </div>
+                                    <input 
+                                        type="tel" 
+                                        class="w-full pl-12 pr-4 py-2.5 rounded-xl border border-gray-300 focus:ring-2 focus:ring-green-500/20 focus:border-green-500 transition-colors" 
+                                        wire:model="phone" 
+                                    />
+                                    @error('phone')
+                                        <p class="text-sm text-red-600 flex items-center gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                            </svg>
+                                            {{ $message }}
+                                        </p>
+                                    @enderror
+                                    <p class="text-xs text-gray-500">{{ __('general.phone_format_international') }}</p>
+                                </div>
                                 <label class="block text-sm font-medium text-gray-700">
                                     {{ __('general.email') }} <span class="text-red-500">*</span>
                                 </label>
